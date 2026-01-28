398aa2e43edf5abe5d210862fff9a451126d7f2d
DDas12345 <das.debanshika@gmail.com>
2026-01-28 14:22:21 +0530

    class 6

--- WAP_LECTURE/class_6.js
+++ WAP_LECTURE/class_6.js
@@ -4,6 +4,11 @@ let student = {
     address: "Chhattisgarh",
 }
 
+let subjects = {
+    WAP : "JS",
+    DSA : "Python"
+}
+
 // let key = Object.keys(student)
 // console.log(key)
 // console.log(key.length)
@@ -20,19 +25,50 @@ let student = {
 //     console.log(key, value)
 // }
 
-let std = [
-    {
-        name: "Debanshika",
-        age: 18,
-        address: "Chhattisgarh"
-    }, 
+// let std = [
+//     {
+//         name: "Debanshika",
+//         age: 18,
+//         address: "Chhattisgarh"
+//     }, 
 
-    {
-        name: "John",
-        age: 20,
-        address: "Madhya Pradesh"
-    }
-]
+//     {
+//         name: "John",
+//         age: 20,
+//         address: "Madhya Pradesh"
+//     }
+// ]
 
-console.log(std)
-console.log(std.length)
+// console.log(std)
+// console.log(std.length)
+
+// let {name, age} = student
+// console.log(name)
+// console.log(age)
+
+// let arr = [10, 20, 30, 40, 50]
+// console.log(arr)
+// let [a, b, c, d, e] = arr
+// console.log(a)
+
+//spread operator
+// let newArr = [...arr]
+// console.log(newArr)
+
+// let updatedstudent = {
+//     ...student,
+//     age: 19
+// }
+
+// console.log(updatedstudent)
+
+// let finaldata = {...student, ...subjects}
+// console.log(finaldata)
+
+let user = {
+    id: 1,
+    name: 'Debanshika',
+    password: 'Naaginofblack@12345'
+}
+
+let {name, ...secretData} = user
