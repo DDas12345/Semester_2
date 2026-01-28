90d4caa666131b697be4236c93c95b12cb833751
DDas12345 <das.debanshika@gmail.com>
2026-01-28 13:37:17 +0530

    class 6

---
+++ WAP_LECTURE/class_6.js
@@ -0,0 +1,38 @@
+let student = {
+    name: "Debanshika",
+    age: 18,
+    address: "Chhattisgarh",
+}
+
+// let key = Object.keys(student)
+// console.log(key)
+// console.log(key.length)
+
+// let value = Object.values(student)
+// console.log(value)
+
+// console.log(student)
+
+// let keyVal = Object.entries(student)
+// console.log(keyVal)
+
+// for (let [key, value] of keyVal) {
+//     console.log(key, value)
+// }
+
+let std = [
+    {
+        name: "Debanshika",
+        age: 18,
+        address: "Chhattisgarh"
+    }, 
+
+    {
+        name: "John",
+        age: 20,
+        address: "Madhya Pradesh"
+    }
+]
+
+console.log(std)
+console.log(std.length)
